6c4046811d1e9c7ca049babb86a76e4c8c0ea7ec
suhdonghwi <hwidongsuh@gmail.com>
2019-07-20 14:55:03 +0900

    Updates

---
+++ precache-manifest.d837dd9a973103765b2681bce004c758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5eae08b954521d355",
+    "url": "/woosan/static/js/main.6618d4a9.chunk.js"
+  },
+  {
+    "revision": "547d9907227ae919d80b",
+    "url": "/woosan/static/js/runtime~main.7936ea76.js"
+  },
+  {
+    "revision": "e919c84e7c0d624ada15",
+    "url": "/woosan/static/js/2.1d254e2b.chunk.js"
+  },
+  {
+    "revision": "285d437c4857b54083037fcde4a3671d",
+    "url": "/woosan/static/media/logo.285d437c.png"
+  },
+  {
+    "revision": "83f45e67db52f91473aaf89d4151c17b",
+    "url": "/woosan/index.html"
+  }
+];
